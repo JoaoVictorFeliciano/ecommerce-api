d74f7eb68f099f726304cba146bbdce99a502a17
João Victor <felicianojoao777@gmail.com>
2026-04-24 18:00:07 -0300

    adress class

--- ecommerce-api/src/main/java/com/joaofeliciano/ecommerce_api/entity/Adress.java
+++ ecommerce-api/src/main/java/com/joaofeliciano/ecommerce_api/entity/Adress.java
@@ -1,4 +1,46 @@
 package com.joaofeliciano.ecommerce_api.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "adress")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Adress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_adress")
+    private Long id_adress;
+
+    @OneToOne
+    @JoinColumn(name = "id_user", nullable = false)
+    private User user;
+
+    @NotBlank
+    @Column(name = "cep", nullable = false)
+    private String cep;
+
+    @NotBlank
+    @Column(name = "road", nullable = false)
+    private String road;
+
+    @NotBlank
+    @Column(name = "house_number", nullable = false)
+    private String house_number;
+
+    @NotBlank
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    @NotBlank
+    @Column(name = "state", nullable = false)
+    private String state;
 }
