2d1b9dfb9650093ce065eaa3e55cc030faeff617
João Victor <felicianojoao777@gmail.com>
2026-04-27 16:00:36 -0300

    service

---
+++ ecommerce-api/src/main/java/com/joaofeliciano/ecommerce_api/service/ProductService.java
@@ -0,0 +1,4 @@
+package com.joaofeliciano.ecommerce_api.service;
+
+public class ProductService {
+}
--- ecommerce-api/src/main/java/com/joaofeliciano/ecommerce_api/service/UserService.java
+++ ecommerce-api/src/main/java/com/joaofeliciano/ecommerce_api/service/UserService.java
@@ -1,4 +1,41 @@
 package com.joaofeliciano.ecommerce_api.service;
 
+import com.joaofeliciano.ecommerce_api.dto.UserRequestDto;
+import com.joaofeliciano.ecommerce_api.dto.UserResponseDto;
+import com.joaofeliciano.ecommerce_api.entity.User;
+import com.joaofeliciano.ecommerce_api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    private User toEntity(UserRequestDto dto){
+        User user = new User();
+        user.setName(dto.getName());
+        user.setEmail(dto.getEmail());
+        user.setPassword(dto.getPassword());
+        user.setTypeUser(dto.getTypeUser());
+        user.setCreationDateUser(LocalDateTime.now());
+        return user;
+    }
+
+    private UserResponseDto toResponseDto(User user) {
+        return new UserResponseDto(
+                user.getIdUser(),
+                user.getName(),
+                user.getEmail(),
+                user.getTypeUser(),
+                user.getCreationDateUser()
+        );
+    }
 }
